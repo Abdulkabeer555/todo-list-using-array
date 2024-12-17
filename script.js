1230d782d6d91f782d5ab375470a59cf0b5060b1
Abdulkabeer555 <akgct123@gmail.com>
2024-12-17 22:57:11 +0500

    Todo list using array

---
+++ script.js
@@ -0,0 +1,17 @@
+var arr = [];
+var todos = prompt(`Enter your todos`);
+arr.push(todos);
+console.log(arr);
+
+var anotherTodos = prompt(`Another todos`);
+arr.push(anotherTodos);
+console.log(arr);
+
+var anotherTodos2 = prompt(`Another todos`);
+arr.push(anotherTodos2);
+console.log(arr);
+
+console.log('Total todos: ' + arr.length);
+
+
+
